5cfaa5730b0787d0373d320e32d4dc5bcdb944e7
Theo Mathieu <tmathieu.github@fastmail.com>
2023-05-16 13:15:31 +0200

    feat: added hsl function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 
-use colors_transform::Rgb;
+use colors_transform::{Color, Rgb};
 use pyo3::wrap_pyfunction;
 
 #[pyfunction]
@@ -30,10 +30,22 @@ fn get_hex_from_rgb(r: f32, g: f32, b: f32) -> PyResult<String> {
     Ok(rgb.to_css_hex_string())
 }
 
+#[pyfunction]
+fn get_hsl_from_rgb(r: f32, g: f32, b: f32) -> PyResult<Vec<f32>> {
+    let rgb = Rgb::from(r, g, b);
+    let hsl = rgb.to_hsl();
+    Ok(vec![
+        hsl.get_hue(),
+        hsl.get_saturation(),
+        hsl.get_lightness(),
+    ])
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn color_palette_extract(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(extract_from_bytes, m)?)?;
     m.add_function(wrap_pyfunction!(get_hex_from_rgb, m)?)?;
+    m.add_function(wrap_pyfunction!(get_hsl_from_rgb, m)?)?;
     Ok(())
 }
